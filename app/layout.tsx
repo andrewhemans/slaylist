ad15ec7598f99318163c8d0cb69e1cc67679fdf9
Andrew Hemans <andrewhemans@Andrews-Mac-mini.local>
2024-01-21 13:27:49 -0800

    added social preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,24 @@ export default function RootLayout({ children }: { children: any }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://slaylist.dev" />
+        <meta name="twitter:title" content="slaylist" />
+        <meta
+          name="twitter:description"
+          content="a custom daylist image generator"
+        />
+        <meta name="twitter:image" content="/preview.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="slaylist" />
+        <meta
+          property="og:description"
+          content="a custom daylist image generator"
+        />
+        <meta property="og:site_name" content="slaylist" />
+        <meta property="og:url" content="https://slaylist.dev" />
+        <meta property="og:image" content="/preview.png" />
       </head>
       <body>
         <MantineProvider defaultColorScheme="dark" theme={theme}>
